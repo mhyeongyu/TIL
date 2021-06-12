16906294132f747afd8959323a4544cc8ea03fcc
mhyeongyu <gusrb4805@icloud.com>
2021-06-12 20:47:07 +0900

    20210612

--- DeepLearning/Pytorch/Pytorch_VGG16Net.py
+++ DeepLearning/Pytorch/Pytorch_VGG16Net.py
@@ -1,3 +1,9 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
 import numpy as np
 import warnings
 warnings.filterwarnings('ignore')
@@ -13,48 +19,57 @@ from torchvision import transforms
 import torchvision.models as models
 
 
+# In[2]:
+
+
+device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
+
+
+# In[3]:
+
+
 transform = transforms.Compose([
     transforms.ToTensor()
 #     transforms.Normalize([0.5, 0.5, 0.5], [0.5, 0.5, 0.5]),
 #     transforms.Resize((224, 224))
 ])
 
+
+# In[4]:
+
+
 train_dataset = datasets.CIFAR10(root='./CIFAR10',
-                              train=True,
-                              download=True,
-                              transform=transform)
+                                 train=True,
+                                 download=True,
+                                 transform=transform)
 
 test_dataset = datasets.CIFAR10(root='./CIFAR10',
                              train=False,
                              download=True,
                              transform=transform)
 
+
+# In[5]:
+
+
 train_loader = torch.utils.data.DataLoader(train_dataset,
-                                           batch_size=512,
+                                           batch_size=128,
                                            shuffle=True)
 
 test_loader = torch.utils.data.DataLoader(test_dataset,
-                                          batch_size=512,
+                                          batch_size=128,
                                           shuffle=False)
 
-device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
 
-# for X_train, y_train in train_loader:
-#     break
-    
-# for i in range(2):
-#     plt.imshow(np.transpose(X_train[i], (1,2,0)))
-#     plt.axis('off')
-#     plt.show()
-    
-# print(X_train.shape)
+# In[6]:
+
 
 classes = ('plane', 'car', 'bird', 'cat',
            'deer', 'dog', 'frog', 'horse', 'ship', 'truck')
 
 model = models.vgg16().to(device)
 criterion = nn.CrossEntropyLoss().cuda()
-optimizer = torch.optim.Adam(model.parameters(), lr=0.01)
+optimizer = torch.optim.Adam(model.parameters(), lr=0.001)
 
 epochs = 10
 
@@ -77,20 +92,35 @@ for epoch in tqdm(range(epochs)):
             running_loss = 0.0
 print('Finish')
 
+
+# In[7]:
+
+
 dataiter = iter(test_loader)
 images, labels = dataiter.next()
+images, labels = images.to(device), labels.to(device)
 outputs = model(images)
+
+
+# In[8]:
+
 
 _, predicted = torch.max(outputs, 1)
 
 print('Predicted: ', ' '.join('%5s' % classes[predicted[j]]
                               for j in range(4)))
 
+
+# In[9]:
+
+
 correct = 0
 total = 0
+
 with torch.no_grad():
     for data in test_loader:
         images, labels = data
+        images, labels = images.to(device), labels.to(device)
         outputs = model(images)
         _, predicted = torch.max(outputs.data, 1)
         total += labels.size(0)
@@ -99,12 +129,17 @@ with torch.no_grad():
 print('Accuracy of the network on the 10000 test images: %d %%' % (
     100 * correct / total))
 
+
+# In[10]:
+
+
 class_correct = list(0. for i in range(10))
 class_total = list(0. for i in range(10))
 
 with torch.no_grad():
     for data in test_loader:
         images, labels = data
+        images, labels = images.to(device), labels.to(device)
         outputs = model(images)
         _, predicted = torch.max(outputs, 1)
         c = (predicted == labels).squeeze()
